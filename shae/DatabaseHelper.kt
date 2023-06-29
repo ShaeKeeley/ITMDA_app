250c3892d76932cc0dc9b5d09aaccbd7a9849ec6
ShaeKeeley <136177512+ShaeKeeley@users.noreply.github.com>
2023-06-29 15:05:40 +0200

    Update DatabaseHelper.kt

--- shae/DatabaseHelper.kt
+++ shae/DatabaseHelper.kt
@@ -1,29 +1,27 @@
 package com.example.insuranceapp
 
-import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
-
-
 
 class DatabaseHelper(context: Context) :
     SQLiteOpenHelper(context, "Userdata", null, 1) {
     override fun onCreate(po: SQLiteDatabase?) {
-        po?.execSQL("CREATE TABLE Userdata (username TEXT PRIMARY KEY, password TEXT, Email TEXT, PhoneNumber TEXT, PolicyNumber TEXT, CarModel TEXT, Year TEXT, LicencePlate TEXT, DateInsured TEXT, Description TEXT)")
+        po?.execSQL("CREATE TABLE Userdata (username TEXT PRIMARY KEY, password TEXT," +
+                " Email TEXT, PhoneNumber TEXT, date TEXT, time TEXT, PostcodeSuburb TEXT, Address TEXT, description TEXT, image BLOB, claimImg BLOB)")
     }
 
     override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
         p0?.execSQL("DROP TABLE IF EXISTS Userdata")
     }
-
+    //insert
     fun insertdata(
         username: String,
         password: String,
         Email: String,
-        PhoneNumber: String,
-        PolicyNumber: String
+        PhoneNumber: String
     ): Boolean {
         val p0 = this.writableDatabase
         val values = ContentValues()
@@ -31,24 +29,6 @@ class DatabaseHelper(context: Context) :
         values.put("password", password)
         values.put("Email", Email)
         values.put("PhoneNumber", PhoneNumber)
-        values.put("PolicyNumber", PolicyNumber)
-        val result = p0.insert("Userdata", null, values)
-        return result != -1L // Return true if the insertion was successful
-    }
-    fun insertdata2(
-        CarModel : String,
-        Year: String,
-        LicencePlate: String,
-        DateInsured: String,
-        Description: String
-    ): Boolean {
-        val p0 = this.writableDatabase
-        val values = ContentValues()
-        values.put("username", CarModel)
-        values.put("Year", Year)
-        values.put("LicencePlate", LicencePlate)
-        values.put("DateInsured", DateInsured)
-        values.put("Description", Description)
         val result = p0.insert("Userdata", null, values)
         if (result == 1.toLong()) {
             return false
@@ -56,7 +36,44 @@ class DatabaseHelper(context: Context) :
         return true
     }
 
+    fun insertdata2(
+        CarModel : String,
+        Year: String,
+        LicencePlate: String,
+        DateInsured: String,
+    ): Boolean {
+        val p0 = this.writableDatabase
+        val values = ContentValues()
+        values.put("username", CarModel)
+        values.put("Year", Year)
+        values.put("LicencePlate", LicencePlate)
+        values.put("DateInsured", DateInsured)
+        val result = p0.insert("Userdata", null, values)
+        if (result == 1.toLong()) {
+            return false
+        }
+        return true
+    }
 
+    fun insertData3(username: String, desc: String, imageBytes: ByteArray?) {
+        val db = this.writableDatabase
+        val values = ContentValues()
+        values.put("description", desc)
+        values.put("claimsImg", imageBytes)
+        db.update("Userdata", values, "username = ?", arrayOf(username))
+    }
+    fun insertData4(db: SQLiteDatabase?, username: String, date1: String, time1: String) {
+        db?.execSQL("UPDATE Userdata SET date = '$date1', time = '$time1' WHERE username = '$username'")
+    }
+    fun insertData5(db: SQLiteDatabase?, username: String, PostcodeSuburb: String, Address1: String) {
+        db?.execSQL("UPDATE Userdata SET PostcodeSuburb = '$PostcodeSuburb', Address = '$Address1' WHERE username = '$username'")
+    }
+    fun insertData6(username: String, imageBytes: ByteArray?) {
+        val db = this.writableDatabase
+        val values = ContentValues()
+        values.put("image", imageBytes)
+        db.update("Userdata", values, "username = ?", arrayOf(username))
+    }
     fun checkuserpass(username: String, password: String): Boolean {
         val p0 = this.writableDatabase
         val query = "select * from Userdata where username = '$username' and password='$password'"
@@ -69,27 +86,9 @@ class DatabaseHelper(context: Context) :
         return true
     }
 
-    @SuppressLint("Range")
-    fun retrieveData(username2: String): List<String>  {
-        val dataList = mutableListOf<String>()
-        val p0 = this.readableDatabase
-        val query = "SELECT username, Email, PhoneNumber, PolicyNumber FROM Userdata WHERE username = '$username2' "
-        val cursor = p0.rawQuery(query, null)
-
-        if (cursor.moveToFirst()) {
-            val username = cursor.getString(cursor.getColumnIndex("username"))
-            val email = cursor.getString(cursor.getColumnIndex("Email")) // Corrected column name
-            val phoneNumber = cursor.getString(cursor.getColumnIndex("PhoneNumber")) // Corrected column name
-            val policyNumber = cursor.getString(cursor.getColumnIndex("PolicyNumber"))
-
-            dataList.add(username)
-            dataList.add(email)
-            dataList.add(phoneNumber)
-            dataList.add(policyNumber)
-
-            cursor.close()
-            return dataList
-        }
-        return dataList // Return an empty list if no data is found
+    fun getText(): Cursor? {
+        val db = this.readableDatabase
+        val query = "SELECT username, Email, PhoneNumber FROM Userdata"
+        return db.rawQuery(query, null)
     }
-    }
+}
